76800768422a851ee8a8c5eb2cac46456ff4d177
Huy Hoang <hoangvh93@gmail.com>
2019-07-24 11:27:03 -0400

    day 19-20

---
+++ Day20_Sorting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day20_Sorting
+{
+    class Program
+    {
+        static void swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+    
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            int numberOfSwaps = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        swap(ref a[j], ref a[j + 1]);
+                        numberOfSwaps++;
+                    }
+                }
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Array is sorted in {0} swaps.", numberOfSwaps);
+            Console.WriteLine("First Element: {0}", a[0]);
+            Console.WriteLine("Last Element: {0}", a[n - 1]);
+        }
+    }
+}
+
